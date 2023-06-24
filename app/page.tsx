19a067309eddd0a24d99c235e94a8d2efa5f1709
Folareen <sakawahab03@gmail.com>
2023-06-24 12:32:04 +0100

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      GhostGram
+    <main className="flex h-screen flex-col items-center justify-center bg-blue-50">
+      <h1 className="text-blue-600 text-2xl font-sans-serif font-bold">
+        Welcome to GhostGram!
+      </h1>
     </main>
   )
 }
