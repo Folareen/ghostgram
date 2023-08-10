41cf7652df6ef500c30d0eddebe8fb8f744b5bef
Folareen <sakawahab03@gmail.com>
2023-08-10 23:08:37 +0100

    added auth links

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
+import Link from "next/link"
+
+
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-blue-50">
       <h1 className="text-blue-600 text-2xl font-sans-serif font-bold">
         Welcome to GhostGram!
       </h1>
+      <Link href={'/login'}>
+        Login
+      </Link>
+      <Link href={'/signup'}>
+        Signup
+      </Link>
     </main>
   )
 }
