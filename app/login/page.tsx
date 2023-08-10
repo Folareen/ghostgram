fe904b2fa805b3f408e2c88ca48ea512ebf529e8
Folareen <sakawahab03@gmail.com>
2023-08-10 23:48:55 +0100

    temporary login page done

---
+++ app/login/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
+import { toast } from "react-toastify"
+
+export default () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [submitting, setSubmitting] = useState(false)
+
+    const router = useRouter()
+
+    const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+        try {
+            e.preventDefault()
+            setSubmitting(true)
+            await axios.post('/api/login', { username, password })
+            router.push('/profile')
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || error.message)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <input type='text' value={username} onChange={(e) => {
+                    setUsername(e.target.value)
+                }} />
+                <input type='password' value={password} onChange={(e) => {
+                    setPassword(e.target.value)
+                }} />
+                <button type="submit">
+                    {
+                        submitting ? 'submitting...' : 'login'
+                    }
+                </button>
+            </form>
+
+        </div>
+    )
+}
