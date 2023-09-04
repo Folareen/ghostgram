c89106ea7b2a63730719389391b3bec0643da8ea
Folareen <sakawahab03@gmail.com>
2023-09-04 17:30:36 +0100

    created profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
+
+
+export default () => {
+    const [username, setUsername] = useState('')
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await axios.get('/api/username')
+                setUsername(res.data.username)
+            } catch (error: any) {
+                toast.error(error?.response?.data?.message || error?.response?.data?.error || error?.message)
+            } finally {
+                setLoading(false)
+            }
+        })()
+    }, [])
+
+    return <div>
+        {
+            loading ?
+                <p>
+                    loading...
+                </p>
+                :
+                <div>
+                    <p>
+                        {username}
+                    </p>
+                    <div>
+                        {
+                            // profile links
+                        }
+                    </div>
+                </div>
+        }
+
+
+    </div>
+}
