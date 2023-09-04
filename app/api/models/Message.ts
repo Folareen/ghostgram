36f045d8d89db710930b46480cf7b9fe67bec352
Folareen <sakawahab03@gmail.com>
2023-09-04 17:22:33 +0100

    created message model

---
+++ app/api/models/Message.ts
@@ -0,0 +1,17 @@
+import { model, models, Schema } from "mongoose";
+
+const MessageSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    }, receiver: {
+        type: String,
+        ref: 'User',
+        required: true,
+    }
+}, {timestamps: true})
+
+const Message = models.Message || model('Message', MessageSchema);
+
+export default Message
