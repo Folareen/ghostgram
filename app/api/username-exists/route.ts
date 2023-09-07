27919726fd5950cbe9664709f390cf48abbac2be
Folareen <sakawahab03@gmail.com>
2023-09-07 22:58:19 +0100

    check username exists

---
+++ app/api/username-exists/route.ts
@@ -0,0 +1,31 @@
+import connectToDB from "@/app/utils/connectToDB";
+import { NextRequest, NextResponse } from "next/server";
+import User from "../models/User";
+
+connectToDB()
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const reqBody = await req.json()
+        const username = reqBody.username
+        if (username) {
+            const usernameExists = await User.findOne({ username })
+            if (usernameExists) {
+                return NextResponse.json({
+                    username,
+                    status: true,
+                }, { status: 200 })
+            } else {
+                return NextResponse.json({
+                    username,
+                    status: false,
+                }, { status: 404 })
+            }
+        } else {
+            return NextResponse.json({ message: 'Invalid username' }, { status: 400 })
+        }
+    } catch (error: any) {
+        console.log(error.message)
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+    }
+}
