78c6c4a0c7cf1624ae09a1c318197486685c1882
Folareen <sakawahab03@gmail.com>
2023-09-07 22:58:58 +0100

    created send message route

---
+++ app/api/send-message/route.ts
@@ -0,0 +1,32 @@
+import connectToDB from "@/app/utils/connectToDB"
+import { NextRequest, NextResponse } from "next/server"
+import Message from "../models/Message"
+import User from "../models/User"
+
+connectToDB()
+
+export const POST = async (req: NextRequest) => {
+    try {
+        const { receiver, content }: { receiver: string, content: string } = await req.json()
+        if (!receiver || !content) {
+            return NextResponse.json({
+                message: 'Receiver and content is required'
+            }, { status: 400 })
+        }
+        const usernameExists = await User.findOne({})
+        if (!usernameExists) {
+            return NextResponse.json({ message: 'Receiver does not exist' }, { status: 400 })
+        }
+
+        const message = await Message.create({ receiver, content })
+
+        return NextResponse.json({
+            message: 'Message sent'
+        }, { status: 200 })
+
+    } catch (error: any) {
+        console.log(error.message)
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+    }
+}
+
