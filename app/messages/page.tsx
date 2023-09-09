df813d1a76a6069a252259532139143c3dd077a9
Folareen <sakawahab03@gmail.com>
2023-09-09 04:38:59 +0100

    please work!!

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -36,7 +36,7 @@ const Page = () => {
                     <div>
                         {
                             messages.map(({ message }) => (
-                                <p>
+                                <p key={message}>
                                     {message}
                                 </p>
                             ))
